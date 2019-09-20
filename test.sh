52398f3064ffaf35cbce956e90cf592615532ba2
Holmes-Alan <zhisong.liu@connect.polyu.hk>
2019-09-20 15:56:55 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 # for 4x SR, run the following command
 python demo_4x.py
 
+# for 8x SR, run the following command
+python demo_8x.py
+
 # for 16x SR, you can run the above command twice to obtain the results from the paper, 
 # or you can directly run 16x SR as follows,
 python demo_16x.py
